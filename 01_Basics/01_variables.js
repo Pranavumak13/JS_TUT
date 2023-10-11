91399f406d0a9c6b4024f30dcb2a3d7cee2903c7
PRANAV UMAK <97172146+Pranavumak13@users.noreply.github.com>
2023-10-11 07:48:26 +0000

    study - variables and datatypes

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,16 @@
+const accId = 84
+let accEmail = "abc@gmail.com"
+let accName; 
+var accPass = "2222";
+accCity = "Paratwada"
+
+// accId = 1 // const Can't be updated
+accEmail = "xy@xy.com"
+accPass;
+accCity = "Amravati"
+
+// console.log(accId); // Old Method
+console.table([accId,accEmail,accPass, accCity, accName]);
+
+// refer the written notes
+
---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict" // treat all JS code as a newer Versions
+
+// alert(3 + 3) // works in browser's console but we are using nodejs so can;t be run
+
+// 1. string
+// 2. number => 2 to power 53
+// 3. bigint
+// 3. bool
+// 4. null => standalone value
+// 5. undefined => 
+// 6. symbol => to find unique elements
+
+
+// 7*. Object
+
+// typeof → null =⇒ OBJECT
+// typeof → undefined =⇒ UNDEFINED
