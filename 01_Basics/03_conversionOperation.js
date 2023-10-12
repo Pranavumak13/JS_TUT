55f7473d29288a370aa51ed4333d41683dfc4e61
Pranavumak13 <2020bcs077@sggs.ac.in>
2023-10-12 15:34:10 +0530

    style - studied the comparison in detail

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -27,4 +27,56 @@ let isBooleanValue = Boolean(booleanValue);
 console.log(isBooleanValue);
 
 // 1 => true; 0 => false
-// "" => false; "abc" => true
+// "" => false; "abc" => true   
+
+
+
+// ********************* Operations ******************************
+
+// + , - , * , / , % , **
+
+// // Easy Operation
+
+// console.log(2 + 3);
+
+// // But the tricky part is this: 
+
+// console.log("2" + 3);
+// console.log(typeof ("2" + 3));
+
+// console.log(2 + "3");
+// console.log(typeof (2 + "3"));
+
+// console.log("1" + 2 + 3); // if first one is strig then all will be string
+// console.log(typeof ("1" + 2 + 3));
+
+// console.log(1 + 2 + "3");  // first operations then do operations and then concatenation
+// console.log(typeof (1 + 2 + "3"));
+
+
+// // Tricy Operation
+// console.log(+true); // 1
+// console.log(+""); // 0
+
+
+// // To Confuse
+// let n1, n2, n3;
+
+// n1 = n2 = n3 = 11 + 1;
+// console.log(n1, n2, n3);
+
+//Postfix and Prefix Increment Operations
+let num1 = 5;
+let num2 = 10;
+
+// Postfix increment
+console.log(num1++); // 5
+
+// Prefix increment
+console.log(++num1); // 7
+
+// Postfix decrement
+console.log(num2--); // 10
+
+// Prefix decrement
+console.log(--num2); // 8
---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,24 @@
+// // Correct Compaerisons
+// console.log(1 < 2 ); 
+// console.log(1 <= 2 ); 
+// console.log(1 > 2 ); 
+// console.log(1 >= 2 ); 
+// console.log(1 == 2 ); 
+// console.log(1 != 2 ); 
+
+// Ambigious Compaerisons
+// console.log(1 < "2");
+// console.log(1 > "2");
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+
+// Solutions use === and !== , it checks for numbers and datatypes
+
+console.log(null === 0);
+console.log(2 === "2");
