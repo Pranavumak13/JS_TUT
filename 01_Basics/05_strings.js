c05ece32e2a31be5767afb8b388d56bd74481d92
Pranavumak13 <2020bcs077@sggs.ac.in>
2023-10-29 22:59:00 +0530

    study - conversion of different websites | comparison operators | strings | Numbers and Maths

---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,41 @@
+const name = "Pranav"
+const Marks = 100;
+
+// console.log(name + " has " + Marks + " marks in the exam") // old way of writing this
+
+// but now a days we use backticks
+console.log(`${name} has ${Marks} marks in the exam`);
+
+
+// string declarations
+const Name = "Pranav"; // one way
+const newName = new String("Pranav Umak") // another way
+
+console.log(typeof newName);
+console.log(newName.__proto__); // to check the prototype of the string 
+
+// newName has many default funtions with it
+// like
+console.log(newName.length);
+console.log(newName.toLowerCase());
+console.log(newName.toUpperCase());
+console.log(newName.charAt(10));
+console.log(newName.indexOf('a'));
+console.log(newName.substring(0, 5));
+console.log(newName.slice(-11, 2));  // the negative counting starts from initial index only for eg index[0] == index[-11], index[1] == index[-10]
+
+// replace and trim
+const newObjectString = new String("       Pranav   Umak        jhndflkhasd");
+console.log(newObjectString.trim()); // trim only removes to the whitespaces eg \n , intial and end space
+console.log(newName.replace(' ', '-'));
+
+// replace is used to decode the URL
+
+const url = "http//:www.insdnanded.com/about%20us";
+console.log(url.replace('%20', '-'))
+console.log(url.includes('insd'));
+
+// split 
+// converting into an ARRAY
+
+console.log(newName.split(' '));
---
+++ 01_Basics/06_num_and_maths.js
@@ -0,0 +1,38 @@
+const num = 100.00;
+
+// console.log(typeof (num));
+
+const balance = new Number(1030333)
+
+// console.log(typeof (balance));
+// console.log(((balance).toString()).length);
+
+// console.log(balance.toFixed(2));
+
+const numNums = 23.93566
+// console.log(numNums.toPrecision(2));
+
+
+const bigNum = 10000
+// console.log(bigNum.toLocaleString());
+
+
+// --------------- Maths ------------------------------
+
+console.log(Math)
+
+// .abs(), .round(), .min(), .max, .ceil(), .floor(), .sqrt(), .pow(), .random(),
+
+
+// console.log(Math.random());
+// console.log(Math.ceil(Math.random()));
+// console.log(Math.round(Math.random() * 100));
+
+// Chai and code formula
+// I want the range between 10 and 20
+min = 10;
+max = 20;
+
+console.log(Math.random() * (max - min));
+console.log(Math.random() * (max - min) + min);
+console.log(Math.round((Math.random() * (max - min + 1)) + min));
