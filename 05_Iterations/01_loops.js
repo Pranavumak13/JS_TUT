f1ace32d11fed3626c28b6247d8842c6f2e4ddbd
PRANAV UMAK <97172146+Pranavumak13@users.noreply.github.com>
2024-03-18 10:48:57 +0000

    study - loops

---
+++ 05_Iterations/01_loops.js
@@ -0,0 +1,25 @@
+// for loop
+
+for (let index = 1; index < 10; index++){
+    const element = index;
+    console.log(element);
+}
+
+
+for (let i = 0; i < 5; i++) {
+    for (let j = 0; j < i; j++) {
+        // console.log("* ");
+        process.stdout.write("* ")
+    }
+    console.log("\n");
+}
+
+// console.log("--------------");
+
+// let index = 0;
+// while (index<5) {
+//     console.log(index);
+//     index++;
+// }
+
+
